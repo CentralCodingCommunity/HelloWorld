7435a30577f5eb6ff709c1e107495c3de4f5619e
Nicolás A. Ortega <nicolas.ortega.froysa@gmail.com>
2014-10-29 17:52:24 -0500

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,6 +1,10 @@
+// Include the stdio.h file (it's a standard library file)
 #include <stdio.h>
 
+// Main function
 int main() {
+	// Print text
 	printf("Hello, World!\n");
+	// return 0 meaning successful run!
 	return 0;
 }
